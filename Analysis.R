347767c1d5c8f9fc14c06e36bb2251804967a2a6
Simon Leger <simon.leger.66@gmail.com>
2023-08-16 10:32:46 -0300

    Update Analysis.R
    - More plots added

--- Analysis.R
+++ Analysis.R
@@ -7,14 +7,16 @@ data$participant_id <- 1:nrow(data)
 excluded_ids <- c(1, 15, 17)
 data <- data[!data$participant_id %in% excluded_ids, ]
 
-correlation_result_1 <- cor.test(data$participant_auc_l5, data$LDI)
-print(correlation_result_1)
+cor.test(data$participant_auc_l5, data$LDI)
 
-correlation_result_2 <- cor.test(data$participant_auc_l5_scale, data$LDI)
-print(correlation_result_2)
+cor.test(data$participant_auc_l5_scale, data$LDI)
 
-correlation_result_3 <- cor.test(data$l5_REC, data$REC)
-print(correlation_result_3)
+cor.test(data$participant_auc_l5_scale, data$REC)
+
+cor.test(data$l5_REC, data$REC)
+
+cor.test(data$l5_REC, data$LDI)
+
 
 ggplot(data, aes(x = participant_auc_l5, y = LDI)) +
   geom_point() +
@@ -34,6 +36,15 @@ ggplot(data, aes(x = participant_auc_l5_scale, y = LDI)) +
   labs(x = "Area under curve scaled",
        y = "LDI")
 
+ggplot(data, aes(x = participant_auc_l5_scale, y = REC)) +
+  geom_point() +
+  geom_smooth(method = "lm") +   # Adds a linear regression line
+  theme_minimal() +
+  scale_x_continuous(limits = c(0, 1)) +
+  scale_y_continuous(limits = c(0, 1)) +
+  labs(x = "Area under curve scaled",
+       y = "REC")
+
 ggplot(data, aes(x = l5_REC, y = REC)) +
   geom_point() +
   geom_smooth(method = "lm") +   # Adds a linear regression line
@@ -42,3 +53,21 @@ ggplot(data, aes(x = l5_REC, y = REC)) +
   scale_y_continuous(limits = c(0, 1)) +
   labs(x = "Logistic5 Max - Min",
        y = "REC")
+
+ggplot(data, aes(x = l5_REC, y = LDI)) +
+  geom_point() +
+  geom_smooth(method = "lm") +   # Adds a linear regression line
+  theme_minimal() +
+  scale_x_continuous(limits = c(0, 1)) +
+  scale_y_continuous(limits = c(0, 1)) +
+  labs(x = "Logistic5 Max - Min",
+       y = "LDI")
+
+ggplot(data, aes(x = REC, y = LDI)) +
+  geom_point() +
+  geom_smooth(method = "lm") +   # Adds a linear regression line
+  theme_minimal() +
+  scale_x_continuous(limits = c(0, 1)) +
+  scale_y_continuous(limits = c(0, 1)) +
+  labs(x = "REC",
+       y = "LDI")
