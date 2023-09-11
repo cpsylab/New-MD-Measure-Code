c8fd373949fc9ad778cabc4c30976f76d705ad28
Simon Leger <81974217+sileger@users.noreply.github.com>
2023-09-11 12:27:07 -0300

    update figures

--- scripts/Analysis_combined.R
+++ scripts/Analysis_combined.R
@@ -25,23 +25,27 @@ data1 <- data1 %>% mutate(study = 1)
 data2 <- data2 %>% mutate(study = 2)
 
 data <- bind_rows(data1, data2)
-data <- mutate(data, participant_auc_l5_scale = 1 - participant_auc_l5_scale)
+data <- mutate(data, lmdi = 1 - participant_auc_l5_scale)
 
 # MIXED EFFECTS MODELS TO TEST LDI AND REC ASSOCIATIONS
-ldi_m <- lmer(data = data, scale(LDI) ~ scale(participant_auc_l5_scale) + scale(l5_REC) + (1|study))
-ldi_rs_m <- lmer(data = data, LDI ~ participant_auc_l5_scale + l5_REC + (1 + participant_auc_l5_scale + l5_REC|study))
-rec_m <- lmer(data = data, REC ~ participant_auc_l5_scale + l5_REC + (1|study))
+ldi_m <- lmer(data = data, scale(LDI) ~ scale(lmdi) + scale(l5_REC) + (1|study))
+ldi_rs_m <- lmer(data = data, LDI ~ lmdi + l5_REC + (1 + lmdi + l5_REC|study))
+rec_m <- lmer(data = data, scale(REC) ~ scale(lmdi) + scale(l5_REC) + (1|study))
+lmdi_lrec_m <- lmer(data = data, scale(lmdi) ~ scale(l5_REC) + (1|study))
 
 tab_model(ldi_m)
 tab_model(rec_m)
+tab_model(lmdi_lrec_m)
+summary(ldi_m)
 summary(rec_m)
+summary(lmdi_lrec_m)
 
 # LDI Figure
 #pdf("ldi-fig.pdf", width=5, height=5)
-fig_ldi <- ggplot(data, aes(x=participant_auc_l5_scale, y=LDI, group=study)) + 
+fig_ldi <- ggplot(data, aes(x=lmdi, y=LDI, group=study)) + 
   geom_point(aes(colour=factor(study)),show.legend = FALSE) + 
   geom_smooth(aes(colour=factor(study)), method="lm",show.legend = FALSE) + 
-  xlab("Novel LDI") + 
+  xlab("LMDI") + 
   ylab("Original MST LDI") +
   scale_color_aaas(labels = c("Study 1", "Study 2")) + 
   theme_bw() + 
@@ -65,7 +69,7 @@ fig_ldi
 fig_rec <- ggplot(data, aes(x=l5_REC, y=REC, group=study)) + 
   geom_point(aes(colour=factor(study)),show.legend = FALSE) + 
   geom_smooth(aes(colour=factor(study)), method="lm",show.legend = FALSE) + 
-  xlab("Novel REC") + 
+  xlab("LREC") + 
   ylab("Original MST REC") +
   scale_color_aaas(labels = c("Study 1", "Study 2")) + 
   theme_bw() + 
@@ -86,11 +90,11 @@ fig_rec <- ggplot(data, aes(x=l5_REC, y=REC, group=study)) +
 #dev.off()
 #pdf("ldi-rec-fig.pdf", width=5, height=5)
 # Figure to show new values are decorrelated
-fig_ldi_rec <- ggplot(data, aes(x=participant_auc_l5_scale, y=l5_REC, group=study)) + 
-  geom_point(aes(colour=factor(study))) + 
-  geom_smooth(aes(colour=factor(study)), method="lm") + 
-  xlab("Novel LDI") + 
-  ylab("Novel REC") +
+fig_ldi_rec <- ggplot(data, aes(x=lmdi, y=l5_REC, group=study)) + 
+  geom_point(aes(colour=factor(study)),show.legend = FALSE) + 
+  geom_smooth(aes(colour=factor(study)), method="lm",show.legend = FALSE) + 
+  xlab("LMDI") + 
+  ylab("LREC") +
   scale_color_aaas(labels = c("Study 1", "Study 2")) + 
   theme_bw() + 
   labs(colour = "Study") + 
@@ -108,17 +112,68 @@ fig_ldi_rec <- ggplot(data, aes(x=participant_auc_l5_scale, y=l5_REC, group=stud
     )
   )
 #dev.off()
+fig_ldi_rec
 
-pdf("fig.pdf", width=21, height=7)
-ggarrange(fig_ldi, fig_rec, fig_ldi_rec, labels = c("A", "B", "C"), font.label = list(size = 24), ncol = 3)
-dev.off()
+fig_ldi_oldrec <- ggplot(data, aes(x=lmdi, y=REC, group=study)) + 
+  geom_point(aes(colour=factor(study))) + 
+  geom_smooth(aes(colour=factor(study)), method="lm") + 
+  xlab("LMDI") + 
+  ylab("REC") +
+  scale_color_aaas(labels = c("Study 1", "Study 2")) + 
+  theme_bw() + 
+  labs(colour = "Study") + 
+  theme(
+    axis.text.x = element_text(size = 14),
+    axis.text.y = element_text(size = 14),
+    axis.title.x = element_text(size = 18),
+    axis.title.y = element_text(size = 18),
+    legend.position = c(0.8, 0.15), 
+    legend.title = element_text(size=16),
+    legend.text = element_text(size=14),
+    legend.background = element_rect(
+      colour = "black", 
+      size = 1
+    )
+  )
+
+fig_ldi_oldrec
+
+fig_oldldi_rec <- ggplot(data, aes(x=LDI, y=l5_REC, group=study)) + 
+  geom_point(aes(colour=factor(study)),show.legend = FALSE) + 
+  geom_smooth(aes(colour=factor(study)), method="lm",show.legend = FALSE) + 
+  xlab("Original MST LDI") + 
+  ylab("LREC") +
+  scale_color_aaas(labels = c("Study 1", "Study 2")) + 
+  theme_bw() + 
+  labs(colour = "Study") + 
+  theme(
+    axis.text.x = element_text(size = 14),
+    axis.text.y = element_text(size = 14),
+    axis.title.x = element_text(size = 18),
+    axis.title.y = element_text(size = 18),
+    legend.position = c(0.8, 0.15), 
+    legend.title = element_text(size=16),
+    legend.text = element_text(size=14),
+    legend.background = element_rect(
+      colour = "black", 
+      size = 1
+    )
+  )
+
+fig_oldldi_rec
+
+#pdf("fig.pdf", width=21, height=14)
+combined_fig <- ggarrange(fig_ldi, fig_rec, fig_ldi_rec, fig_oldldi_rec,
+                          fig_ldi_oldrec, labels = c("A", "B", "C", "D", "E"),
+                          font.label = list(size = 24), ncol = 3, nrow = 2)
+#dev.off()
 
 combined_fig
 #cor.test(data$participant_auc_l5, data$LDI)
 
-cor.test(data$participant_auc_l5_scale, data$LDI)
+cor.test(data$lmdi, data$LDI)
 
-cor.test(data$participant_auc_l5_scale, data$REC)
+cor.test(data$lmdi, data$REC)
 
 cor.test(data$l5_REC, data$REC)
 
@@ -126,7 +181,7 @@ cor.test(data$l5_REC, data$LDI)
 
 ggscatterstats(
   data = data,
-  x = participant_auc_l5_scale,
+  x = lmdi,
   y = LDI,
   xlab = "Area under curve scaled",
   ylab = "LDI",
@@ -145,7 +200,7 @@ ggplot(data, aes(x = participant_auc_l5, y = LDI)) +
   labs(x = "Area under curve",
        y = "LDI")
 
-ggplot(data, aes(x = participant_auc_l5_scale, y = LDI)) +
+ggplot(data, aes(x = lmdi, y = LDI)) +
   geom_point() +
   geom_smooth(method = "lm") +   # Adds a linear regression line
   theme_minimal() +
@@ -154,7 +209,7 @@ ggplot(data, aes(x = participant_auc_l5_scale, y = LDI)) +
   labs(x = "Area under curve scaled",
        y = "LDI")
 
-ggplot(data, aes(x = participant_auc_l5_scale, y = REC)) +
+ggplot(data, aes(x = lmdi, y = REC)) +
   geom_point() +
   geom_smooth(method = "lm") +   # Adds a linear regression line
   theme_minimal() +
